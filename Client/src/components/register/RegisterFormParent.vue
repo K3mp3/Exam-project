93b5123dd8a835f67b4cd05fc9f76fe7de56c04f
Melvin Kempe <k3mp314@gmail.com>
2024-01-03 14:24:33 +0100

    [Added] check to make sure that the user writes the correct password and email

--- Client/src/components/register/RegisterFormParent.vue
+++ Client/src/components/register/RegisterFormParent.vue
@@ -1,12 +1,55 @@
 <script setup lang="ts">
-    import { ref } from 'vue';
+    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+    import { computed, ref } from 'vue';
+    import { registerUser } from '@/services/registerUser'; 
 
     const name = ref("");
     const email = ref("");
-    const confimEmail = ref("");
+    const confirmEmail = ref("");
     const password = ref("");
     const confirmPassword = ref("");
 
+    const isEmailWrong = ref(false);
+    const isPasswordWrong = ref(false);
+
+    const newUser = computed(() => {
+        return {
+            name: name.value,
+            email: email.value,
+            password: password.value
+        }
+    })
+
+    function checkInputData() {
+        if (email.value === confirmEmail.value) {
+            console.log("hej")
+        } else {
+            console.log("nej");
+            isEmailWrong.value = true;
+            return false;
+        }
+
+        if (password.value === confirmPassword.value) {
+            console.log("hej då")
+        } else {
+            console.log("nej då");
+            isPasswordWrong.value = true;
+            return false;
+        }
+
+        return true;
+    }
+
+    async function handleRegistration() {
+        checkInputData()
+
+        if (checkInputData()) {
+            const response = await registerUser(newUser.value)
+        } else {
+            console.log("duiban");
+            
+        }
+    }
 </script>
 
 <template>
@@ -14,21 +57,25 @@
         <RouterLink to="/" class="router-link"><fontAwesome :icon="['fas', 'chevron-left']" /></RouterLink>
         <h1>Registrera dig</h1>
     </nav>
-    <form>
+    <form @submit.prevent="handleRegistration">
         <label for="name">För- och efternamn</label>
-        <input type="text" name="name" placeholder="För- och efternammn">
+        <input type="text" name="name" placeholder="För- och efternammn" v-model="name">
 
         <label for="email">Email adress</label>
-        <input type="emaiil" name="email" placeholder="namn@mail.com">
+        <input type="email" name="email" placeholder="namn@mail.com" v-model="email" :class="isEmailWrong ? 'input-error' : ''">
+        <p v-if="isEmailWrong"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Vänligen kontrollera email adressen</p>
 
         <label for="email">Bekräfta email adress</label>
-        <input type="email" name="email" placeholder="namn@mail.com">
+        <input type="email" name="email" placeholder="namn@mail.com" v-model="confirmEmail" :class="isEmailWrong ? 'input-error' : ''">
+        <p v-if="isEmailWrong"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Vänligen kontrollera email adressen</p>
 
         <label for="password">Lösenord</label>
-        <input type="password" name="password" placeholder="Lösenord">
+        <input type="password" name="password" placeholder="Lösenord" v-model="password" :class="isPasswordWrong ? 'input-error' : ''">
+        <p v-if="isEmailWrong"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Vänligen kontrollera email adressen</p>
 
         <label for="password">Bekräfta lösenord</label>
-        <input type="password" name="password" placeholder="Lösenord">
+        <input type="password" name="password" placeholder="Lösenord" v-model="confirmPassword" :class="isPasswordWrong ? 'input-error' : ''">
+        <p v-if="isEmailWrong"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Vänligen kontrollera email adressen</p>
 
         <button type="submit">Registrera</button>
     </form>
--- Client/src/services/registerUser.ts
+++ Client/src/services/registerUser.ts
@@ -4,6 +4,7 @@ import axios from "axios";
 const BASE_URL = "http://localhost:3000";
 
 export async function registerUser(user: IUser) {
+    console.log(user);
     try {
         const response = await axios.post<IUser>(`${BASE_URL}/users/createUser`, user);
         return response.data;
