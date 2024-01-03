cf0f4727f446094eb7d015c3edaf1992c1369c50
Melvin Kempe <k3mp314@gmail.com>
2024-01-03 13:34:01 +0100

    [Removed] unused files

---
+++ Client/src/components/register/RegisterFormParent.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+
+    const name = ref("");
+    const email = ref("");
+    const confimEmail = ref("");
+    const password = ref("");
+    const confirmPassword = ref("");
+
+</script>
+
+<template>
+    <nav class="register-nav">
+        <RouterLink to="/" class="router-link"><fontAwesome :icon="['fas', 'chevron-left']" /></RouterLink>
+        <h1>Registrera dig</h1>
+    </nav>
+    <form>
+        <label for="name">För- och efternamn</label>
+        <input type="text" name="name" placeholder="För- och efternammn">
+
+        <label for="email">Email adress</label>
+        <input type="emaiil" name="email" placeholder="namn@mail.com">
+
+        <label for="email">Bekräfta email adress</label>
+        <input type="email" name="email" placeholder="namn@mail.com">
+
+        <label for="password">Lösenord</label>
+        <input type="password" name="password" placeholder="Lösenord">
+
+        <label for="password">Bekräfta lösenord</label>
+        <input type="password" name="password" placeholder="Lösenord">
+
+        <button type="submit">Registrera</button>
+    </form>
+    
+    <div class="blue-line"></div>
+
+    <div class="text-form-container">
+        <p>Har du redan ett konto? <RouterLink to="/" class="router-link-text">Logga in här</RouterLink></p>
+    <p>Har du en verkstad och vill registrera dig? <RouterLink to="/" class="router-link-text">Klicka här</RouterLink></p>
+    </div>
+</template>
--- Client/src/router/index.ts
+++ Client/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ConsumerLandingView from '@/views/ConsumerLandingView.vue'
 import LayoutVue from '@/views/Layout.vue'
+import RegisterViewVue from '@/views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,12 @@ const router = createRouter({
           path: '/',
           name: "landing page",
           component: ConsumerLandingView,
+          meta: {index: true}
+        },
+        {
+          path: '/register',
+          name: "register form",
+          component: RegisterViewVue,
           meta: {index: true}
         }
       ]
