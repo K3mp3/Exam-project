698cd49a40c48709c2fff79ca7bb03ae3096fded
Melvin Kempe <k3mp314@gmail.com>
2024-01-08 14:17:30 +0100

    [Added] validation for the input forms

--- Client/src/components/register/RegisterFormMobile.vue
+++ Client/src/components/register/RegisterFormMobile.vue
@@ -13,6 +13,9 @@
     const isEmailWrong = ref(false);
     const isPasswordWrong = ref(false);
     const isNotBothNames = ref(false);
+    const disableRegisterBtn = ref(true);
+    const isPasswordWeak = ref(false);
+    const isConfirmPasswordWeak = ref(false);
 
     const isDialog = computed(() => useShowPopUp().showPopUp)
 
@@ -26,14 +29,26 @@
     })
 
     function checkInputData() {
-        if (!email.value === !confirmEmail.value) {
-            isEmailWrong.value = true;
-        } 
+        console.log("hello")
+        if (name.value === "" || email.value === "" || confirmEmail.value === "" || password.value === "" || confirmPassword.value === "") {
+            disableRegisterBtn.value = true;
+            return;
+        } else {
+            disableRegisterBtn.value = false;
+        }
 
-        if (!password.value === !confirmPassword.value) {
-            isPasswordWrong.value = true;
+        if (email.value === confirmEmail.value) {
+            isEmailWrong.value = false;
+        } else {
+            return isEmailWrong.value = true;
+        }
+
+        if (password.value === confirmPassword.value) {
+            isPasswordWrong.value = false;
             console.log(isPasswordWrong.value)
-        } 
+        } else {
+            return isPasswordWrong.value = true;
+        }
 
         const nameRegex = /^[\p{L}]+ [\p{L}]+$/u;
         if (!nameRegex.test(name.value)) {
@@ -44,13 +59,22 @@
         return true;
     }
 
+    function checkPasswordStrength(type: string) {
+
+        if (type === "password") {
+            isPasswordWeak.value = password.value.length < 5;
+        } else {
+            isConfirmPasswordWeak.value = confirmPassword.value.length < 5;
+        }
+    }
+
     async function handleRegistration() {
         checkInputData()
 
         if (checkInputData()) {
             const response = await registerUser(newUser.value)
         } else {
-            
+            console.log("error");
         }
     }
 </script>   
@@ -63,26 +87,28 @@
         </div>
         <form @submit.prevent="handleRegistration" class="mobile-register-form">
             <label for="name">För- och efternamn</label>
-            <input type="text" name="name" placeholder="För- och efternammn" v-model="name" :class="isNotBothNames ? 'input-error' : ''">
-            <p v-if="isEmailWrong"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Vänligen kontrollera så att både för- och efternamn finns med!</p>
+            <input type="text" name="name" placeholder="För- och efternammn" v-model="name" :onChange="checkInputData" :class="isNotBothNames ? 'input-error' : ''">
+            <!-- <p v-if="isEmailWrong"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Vänligen kontrollera så att både för- och efternamn finns med!</p> -->
 
             <label for="email">Email adress</label>
-            <input type="email" name="email" placeholder="namn@mail.com" v-model="email" :class="isEmailWrong ? 'input-error' : ''">
+            <input type="email" name="email" placeholder="namn@mail.com" v-model="email" :onChange="checkInputData" :class="isEmailWrong ? 'input-error' : ''">
             <p v-if="isEmailWrong"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Vänligen kontrollera email adressen!</p>
 
             <label for="email">Bekräfta email adress</label>
-            <input type="email" name="email" placeholder="namn@mail.com" v-model="confirmEmail" :class="isEmailWrong ? 'input-error' : ''">
+            <input type="email" name="email" placeholder="namn@mail.com" v-model="confirmEmail" :onChange="checkInputData" :class="isEmailWrong ? 'input-error' : ''">
             <p v-if="isEmailWrong"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Vänligen kontrollera email adressen!</p>
 
             <label for="password">Lösenord</label>
-            <input type="password" name="password" placeholder="Lösenord" v-model="password" :class="isPasswordWrong ? 'input-error' : ''">
-            <p v-if="isEmailWrong"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Vänligen kontrollera lösenorder!</p>
+            <input type="password" name="password" placeholder="Lösenord" v-model="password" @onChange="checkInputData" @input="checkPasswordStrength('password')" :class="{ 'input-error': isPasswordWrong, 'input-password-weak': isPasswordWeak }">
+            <p class="warning-text" v-if="isPasswordWeak"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Lösenordet är svagt! Överväg att använda ett säkrare</p>
+            <p v-if="isPasswordWrong"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Vänligen kontrollera lösenorder!</p>
 
             <label for="password">Bekräfta lösenord</label>
-            <input type="password" name="password" placeholder="Lösenord" v-model="confirmPassword" :class="isPasswordWrong ? 'input-error' : ''">
-            <p v-if="isEmailWrong"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Vänligen kontrollera lösenorder!</p>
+            <input type="password" name="password" placeholder="Lösenord" v-model="confirmPassword" @onChange="checkInputData" @input="checkPasswordStrength('confirmPassword')" :class="{ 'input-error': isPasswordWrong, 'input-password-weak': isConfirmPasswordWeak }">
+            <p class="warning-text" v-if="isConfirmPasswordWeak"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Lösenordet är svagt! Överväg att använda ett säkrare</p>
+            <p v-if="isPasswordWrong"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Vänligen kontrollera lösenorder!</p>
 
-            <button type="submit" class="register-mobile-button">Registrera</button>
+            <button type="submit" :disabled="disableRegisterBtn" :class="disableRegisterBtn ? 'register-mobile-button-disable' : 'register-mobile-button'">Registrera</button>
         </form>
 
         <div class="blue-line"></div>
--- Client/src/components/registerRepairShop/RegisterRepairShopMobile.vue
+++ Client/src/components/registerRepairShop/RegisterRepairShopMobile.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-    import { computed, onMounted, ref, toValue } from 'vue';
+    import { computed, ref } from 'vue';
     import { registerRepairShop } from '../../services/registerUser'; 
     import DialogBox from "../popup/DialogBox.vue";
-    // import { useShowPopUp } from '@/stores/ShowPopUpStore';
+    import { useShowPopUp } from '@/stores/ShowPopUpStore';
 
     const name = ref("");
     const location = ref("");
@@ -14,9 +14,11 @@
 
     const isEmailWrong = ref(false);
     const isPasswordWrong = ref(false);
-    const isNotBothNames = ref(false);
+    const disableRegisterBtn = ref(true);
+    const isPasswordWeak = ref(false);
+    const isConfirmPasswordWeak = ref(false);
 
-    // const isDialog = computed(() => useShowPopUp().showPopUp)
+    const isDialog = computed(() => useShowPopUp().showPopUp)
 
     const newUser = computed(() => {
         return {
@@ -30,6 +32,13 @@
     })
 
     function checkInputData() {
+        if (name.value === "" || location.value === "" || phoneNumber.value === "" || email.value === "" || confirmEmail.value === "" || password.value === "" || confirmPassword.value === "") {
+            disableRegisterBtn.value = true;
+            return;
+        } else {
+            disableRegisterBtn.value = false;
+        }
+
         if (email.value === confirmEmail.value) {
             isEmailWrong.value = false;
         } else {
@@ -44,6 +53,14 @@
         }
 
         return true;
+    }
+
+    function checkPasswordStrength(type: string) {
+        if (type === "password") {
+            isPasswordWeak.value = password.value.length < 5;
+        } else {
+            isConfirmPasswordWeak.value = confirmPassword.value.length < 5;
+        }
     }
 
     async function handleRegistration() {
@@ -66,17 +83,15 @@
         </div>
         <form @submit.prevent="handleRegistration" class="mobile-register-form">
             <label for="name">Namn</label>
-            <input type="text" name="name" placeholder="Namn på verkstaden" v-model="name" :class="isNotBothNames ? 'input-error' : ''">
-          
+            <input type="text" name="name" placeholder="Namn på verkstaden" v-model="name">
 
             <label for="location">Kommun</label>
             <select name="location" class="mobile-register-form-select" v-model="location" :key="location">
                 <option value="Sundsvall">Sundsvall</option>
             </select>
 
-
             <label for="phone-number">Telefonnummer</label>
-            <input type="tel" name="phone-number" placeholder="Telefonnummer till din verkstad" v-model="phoneNumber" :class="isNotBothNames ? 'input-error' : ''">
+            <input type="tel" name="phone-number" placeholder="Telefonnummer till din verkstad" v-model="phoneNumber">
            
 
             <label for="email">Email adress</label>
@@ -86,13 +101,15 @@
             <label for="email">Bekräfta email adress</label>
             <input type="email" name="email" placeholder="namn@mail.com" v-model="confirmEmail" :class="isEmailWrong ? 'input-error' : ''">
             <p v-if="isEmailWrong"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Vänligen kontrollera email adressen!</p>
-
+            
             <label for="password">Lösenord</label>
-            <input type="password" name="password" placeholder="Lösenord" v-model="password" :class="isPasswordWrong ? 'input-error' : ''">
+            <input type="password" name="password" placeholder="Lösenord" v-model="password" @onChange="checkInputData" @input="checkPasswordStrength('password')" :class="{ 'input-error': isPasswordWrong, 'input-password-weak': isPasswordWeak }">
+            <p class="warning-text" v-if="isPasswordWeak"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Lösenordet är svagt! Överväg att använda ett säkrare</p>
             <p v-if="isPasswordWrong"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Vänligen kontrollera lösenorder!</p>
 
             <label for="password">Bekräfta lösenord</label>
-            <input type="password" name="password" placeholder="Lösenord" v-model="confirmPassword" :class="isPasswordWrong ? 'input-error' : ''">
+            <input type="password" name="password" placeholder="Lösenord" v-model="confirmPassword" @onChange="checkInputData" @input="checkPasswordStrength('confirmPassword')" :class="{ 'input-error': isPasswordWrong, 'input-password-weak': isConfirmPasswordWeak }">
+            <p class="warning-text" v-if="isConfirmPasswordWeak"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Lösenordet är svagt! Överväg att använda ett säkrare</p>
             <p v-if="isPasswordWrong"><fontAwesome :icon="['fas', 'triangle-exclamation']" />Vänligen kontrollera lösenorder!</p>
 
             <button type="submit" class="register-mobile-button">Registrera</button>
@@ -105,6 +122,6 @@
             <p>Har du en verkstad och vill registrera dig? <RouterLink to="/" class="router-link-text">Klicka här</RouterLink></p>
         </div>
         
-        <!-- <DialogBox v-if="isDialog"></DialogBox> -->
+        <DialogBox v-if="isDialog"></DialogBox>
     </div>
 </template>
