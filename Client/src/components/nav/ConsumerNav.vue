87cfd17261921b4fa66c433bc0a65f481f5e1584
Melvin Kempe <k3mp314@gmail.com>
2024-01-10 19:15:19 +0100

    [Added] a color to the desktop nav on scroll

--- Client/src/components/nav/ConsumerNav.vue
+++ Client/src/components/nav/ConsumerNav.vue
@@ -1,89 +1,65 @@
 <script setup lang="ts">
-    import { computed, onMounted, ref } from 'vue';
-    import ConsumerNavMobile from "./ConsumerNavMobile.vue";
-    import ConsumerNavTablet from './ConsumerNavTablet.vue';
-    import RegisterDialog from '../register/RegisterDialog.vue';
-    import { useShowRegisterDialog } from '@/stores/showRegisterDialog';
-    import SignInDialog from '../signIn/SignInDialog.vue';
-    import { useShowSignInDialog } from '../../stores/showSignInDialog';
-import RegisterRepairShopDialog from '../registerRepairShop/RegisterRepairShopDialog.vue';
-import { useShowRepairShopDialog } from '@/stores/useShowRepairShopDialog';
+import { useShowRegisterDialog } from '@/stores/showRegisterDialog'
+import { useShowSignInDialog } from '@/stores/showSignInDialog'
+import { useShowRepairShopDialog } from '@/stores/useShowRepairShopDialog'
+import { computed, onMounted, ref } from 'vue'
+import RegisterDialog from '../register/RegisterDialog.vue'
+import RegisterRepairShopDialog from '../registerRepairShop/RegisterRepairShopDialog.vue'
+import SignInDialog from '../signIn/SignInDialog.vue'
+import ConsumerNavDesktop from './ConsumerNavDesktop.vue'
+import ConsumerNavMobile from './ConsumerNavMobile.vue'
+import ConsumerNavTablet from './ConsumerNavTablet.vue'
 
-    const navMobile = ref(true);
-    const navTablet = ref(false);
-    const navDesktop = ref(false);
+const navMobile = ref(true)
+const navTablet = ref(false)
+const navDesktop = ref(false)
 
-    const isRegister = computed(() => useShowRegisterDialog().isRegisterDialog);
-    const isSignIn = computed(() => useShowSignInDialog().isSignInDialog);
-    const isRepairShopDialog = computed(() => useShowRepairShopDialog().isRepairShopDialog);
+const isRepairShopDialog = computed(() => useShowRepairShopDialog().isRepairShopDialog)
+const isRegister = computed(() => useShowRegisterDialog().isRegisterDialog)
+const isSignIn = computed(() => useShowSignInDialog().isSignInDialog)
 
+let width = document.documentElement.clientWidth
 
-    let width = document.documentElement.clientWidth;
+function updateScreenSize() {
+  window.addEventListener('resize', updateScreenSize)
+  width = document.documentElement.clientWidth
 
-    function updateScreenSize() {
-        window.addEventListener("resize", updateScreenSize);
-        width = document.documentElement.clientWidth;
+  if (width < 700) {
+    navMobile.value = true
 
-        if (width < 700) {
-            navMobile.value = true;
+    navTablet.value = false
+    navDesktop.value = false
+    return
+  }
 
-            navTablet.value = false;
-            navDesktop.value = false;
-            return;
-        }
+  if (width > 1481) {
+    navDesktop.value = true
+    navTablet.value = false
+    navMobile.value = false
+    return
+  } else {
+    navDesktop.value = false
+  }
 
-        if (width > 1481) {
-            navDesktop.value = true;
-            navTablet.value = false;
-            navMobile.value = false;
-            return;
-        } else {
-            navDesktop.value = false;
-        }
-        
-        if (width > 699) {
-            navTablet.value = true;
-            navMobile.value = false;
-        } else {
-            navTablet.value = false;
-        }
-    }
+  if (width > 699) {
+    navTablet.value = true
+    navMobile.value = false
+  } else {
+    navTablet.value = false
+  }
+}
 
-    function showRegisterForm() {
-        const showRegisterDialog = useShowRegisterDialog();
-        showRegisterDialog.showRegisterDialogForm(!isRegister.value);
-    }
-
-    function showSignInForm() {
-        const showSignInDialog = useShowSignInDialog();
-        showSignInDialog.showSignInDialogForm(!isSignIn.value);
-    }
-
-    onMounted(() => {
-        updateScreenSize();
-    })
+onMounted(() => {
+  updateScreenSize()
+})
 </script>
 
 <template>
-    <ConsumerNavMobile v-if="navMobile"></ConsumerNavMobile>
-    <ConsumerNavTablet v-if="navTablet"></ConsumerNavTablet>
-   
-    <div class="nav-parent-container" v-if="navDesktop">
-        <div class="nav-child-container left">
-            <h1>Ways</h1>
-        </div>
-        <div class="nav-child-container center">
-            <RouterLink to="/" class="router-link"><fontAwesome :icon="['fas', 'house']" /> Hem</RouterLink>
-            <RouterLink to="register" class="router-link"><fontAwesome :icon="['fas', 'address-book']" /> Kontakt</RouterLink>
-            <RouterLink to="register" class="router-link"><fontAwesome :icon="['fas', 'address-card']" /> Om oss</RouterLink>
-            <RouterLink to="register" class="router-link"><fontAwesome :icon="['fas', 'question']" /> FAQ</RouterLink>
-        </div>
-        <div class="nav-child-container right">
-            <button type="button" class="btn-register" @click="showRegisterForm">Registrera</button>
-            <button type="button" class="btn-sign-in" @click="showSignInForm">Logga in</button>
-        </div>
-    </div>
-    <RegisterDialog v-if="isRegister"></RegisterDialog>
-    <RegisterRepairShopDialog v-if="isRepairShopDialog"></RegisterRepairShopDialog>
-    <SignInDialog v-if="isSignIn"></SignInDialog>
+  <ConsumerNavMobile v-if="navMobile"></ConsumerNavMobile>
+  <ConsumerNavTablet v-if="navTablet"></ConsumerNavTablet>
+  <ConsumerNavDesktop v-if="navDesktop"></ConsumerNavDesktop>
+
+  <RegisterDialog v-if="isRegister"></RegisterDialog>
+  <RegisterRepairShopDialog v-if="isRepairShopDialog"></RegisterRepairShopDialog>
+  <SignInDialog v-if="isSignIn"></SignInDialog>
 </template>
---
+++ Client/src/components/nav/ConsumerNavDesktop.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { useShowRegisterDialog } from '@/stores/showRegisterDialog'
+import { useShowSignInDialog } from '@/stores/showSignInDialog'
+import { computed, onMounted, ref } from 'vue'
+
+const navScroll = ref(false)
+
+const isRegister = computed(() => useShowRegisterDialog().isRegisterDialog)
+const isSignIn = computed(() => useShowSignInDialog().isSignInDialog)
+
+function showRegisterForm() {
+  const showRegisterDialog = useShowRegisterDialog()
+  showRegisterDialog.showRegisterDialogForm(!isRegister.value)
+}
+
+function showSignInForm() {
+  const showSignInDialog = useShowSignInDialog()
+  showSignInDialog.showSignInDialogForm(!isSignIn.value)
+}
+
+window.addEventListener('scroll', changeNavColor)
+
+function changeNavColor() {
+  if (document.documentElement.scrollTop > 200) navScroll.value = true
+  else {
+    navScroll.value = false
+  }
+}
+
+onMounted(() => {
+  changeNavColor()
+})
+</script>
+
+<template>
+  <div
+    class="nav-parent-container"
+    :style="{ backgroundColor: navScroll ? '#090909de' : 'transparent' }"
+  >
+    <div class="nav-child-container left">
+      <h1>Ways</h1>
+    </div>
+    <div class="nav-child-container center">
+      <RouterLink to="/" class="router-link"
+        ><fontAwesome :icon="['fas', 'house']" /> Hem</RouterLink
+      >
+      <RouterLink to="register" class="router-link"
+        ><fontAwesome :icon="['fas', 'address-book']" /> Kontakt</RouterLink
+      >
+      <RouterLink to="register" class="router-link"
+        ><fontAwesome :icon="['fas', 'address-card']" /> Om oss</RouterLink
+      >
+      <RouterLink to="register" class="router-link"
+        ><fontAwesome :icon="['fas', 'question']" /> FAQ</RouterLink
+      >
+    </div>
+    <div class="nav-child-container right">
+      <button type="button" class="btn-register" @click="showRegisterForm">Registrera</button>
+      <button type="button" class="btn-sign-in" @click="showSignInForm">Logga in</button>
+    </div>
+  </div>
+</template>
