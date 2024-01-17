67d2c0673595592bd06641c1dbd9152c54f1f74b
Melvin Kempe <k3mp314@gmail.com>
2024-01-17 19:14:52 +0100

    [Added] a new array to remove answered messages for the repair shop who has answered to that message. Next step is to create a new route to store that in a secure way

--- Client/src/components/repairShopHome/RepairShopForm.vue
+++ Client/src/components/repairShopHome/RepairShopForm.vue
@@ -7,21 +7,37 @@ import { onMounted, ref } from 'vue'
 import RepairShopMessageContent from './RepairShopMessageContent.vue'
 
 const messages = ref<IUserContact[]>([])
+const unansweredMessages = ref<IUserContact[]>([])
+const answeredMessages = ref<IUserContact[]>([])
 
 async function getMessages() {
   const response = await getContactRepairShops()
-  messages.value = response
+  unansweredMessages.value = response
 
   console.log(response)
 }
 
 async function handleAnswer(answerData: Object) {
+  console.log(unansweredMessages.value)
   const castedAnswerData = answerData as IRepairShopAnswer
 
   console.log('Answer received from child:', castedAnswerData)
 
   // Now you can pass castedAnswerData to the service function
   const response = await answerFromRepairShop(castedAnswerData)
+
+  const answeredMessageIndex = unansweredMessages.value.findIndex(
+    (message) => message._id === castedAnswerData.customerId
+  )
+
+  console.log(unansweredMessages.value)
+
+  if (answeredMessageIndex !== -1) {
+    const [answeredMessage] = unansweredMessages.value.splice(answeredMessageIndex, 1)
+    answeredMessages.value.push(answeredMessage)
+    console.log(unansweredMessages.value)
+    console.log(answeredMessages.value)
+  }
 }
 
 onMounted(() => {
@@ -35,7 +51,7 @@ onMounted(() => {
 
   <form @submit.prevent="handleAnswer" class="repair-shop-requests-form">
     <RepairShopMessageContent
-      v-for="index in messages"
+      v-for="index in unansweredMessages"
       :key="index._id"
       :index="index"
       class="repair-shop-message-content-component"
