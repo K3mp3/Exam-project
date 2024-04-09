008c21d0607ce8cd4a82c306e6501629fe31d91b
Melvin Kempe <k3mp314@gmail.com>
2024-04-09 18:14:59 +0200

    Revert test

--- Client/src/components/repairShopSent/RepairShopSentParent.vue
+++ Client/src/components/repairShopSent/RepairShopSentParent.vue
@@ -1,1 +1,13 @@
+<script setup lang="ts">
+import UserSentTopNav from '../userSent/UserSentTopNav.vue'
+import SentForm from './SentForm.vue'
+</script>
 
+<template>
+  <UserSentTopNav />
+  <div class="repair-shop-home-parent">
+    <div class="repair-shop-signed-in-main">
+      <SentForm />
+    </div>
+  </div>
+</template>
