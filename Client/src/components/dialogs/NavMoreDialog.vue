c7ef347a418b7ce5b252e8f5d82cd94dcf6f7f17
Melvin Kempe <k3mp314@gmail.com>
2024-06-17 19:47:51 +0200

    [Add] mobile nav to signed in users

--- Client/src/components/dialogs/NavMoreDialog.vue
+++ Client/src/components/dialogs/NavMoreDialog.vue
@@ -4,8 +4,12 @@
   <div class="fixed bottom-0 mb-[83px] w-full">
     <div class="max-w-36 p-4 bg-main-40 border-main backdrop-blur-sm m-auto rounded-lg">
       <ul class="text-main flex flex-col gap-4 text-center">
-        <li><fontAwesome :icon="['fas', 'paper-plane']" /> Skickat</li>
-        <li><fontAwesome :icon="['fas', 'user']" /> Profil</li>
+        <li class="flex gap-2 w-full items-center justify-center">
+          <fontAwesome :icon="['fas', 'paper-plane']" /> Skickat
+        </li>
+        <li class="flex gap-2 w-full items-center justify-center">
+          <fontAwesome :icon="['fas', 'user']" /> Profil
+        </li>
       </ul>
     </div>
   </div>
--- Client/src/components/nav/BottomNav.vue
+++ Client/src/components/nav/BottomNav.vue
@@ -1,18 +1,29 @@
 <script setup lang="ts">
+import router from '@/router'
+import { getAuth, signOut } from 'firebase/auth'
 import { ref } from 'vue'
 import NavMoreDialog from '../dialogs/NavMoreDialog.vue'
 
 const activeButton = ref('')
+const auth = getAuth()
+
+function signOutUser() {
+  activeButton.value = 'sign-out'
+
+  signOut(auth).then(() => {
+    router.push('/')
+  })
+}
 </script>
 
 <template>
-  <NavMoreDialog />
+  <NavMoreDialog v-if="activeButton === 'more'" />
   <div class="w-screen p-4 z-10 fixed bottom-0 left-0">
     <div class="flex flex-col px-4 rounded-lg border-main bg-main-40 backdrop-blur-sm gap-8">
       <ul class="flex flex-row justify-center items-center gap-4">
         <li class="activeUrl list-style-none w-full">
-          <button
-            type="button"
+          <RouterLink
+            :to="`/user-home/${auth.currentUser?.uid}`"
             :class="[
               'w-full text-main font-text-light flex flex-col gap-1 p-4',
               activeButton === 'inbox' && 'ease-in-out duration-300 text-active-blue'
@@ -20,7 +31,7 @@ const activeButton = ref('')
             @click="() => (activeButton = 'inbox')"
           >
             <fontAwesome :icon="['fas', 'inbox']" />
-          </button>
+          </RouterLink>
         </li>
         <li class="activeUrl list-style-none w-full">
           <button
@@ -41,7 +52,7 @@ const activeButton = ref('')
               'w-full text-main font-text-light flex flex-col gap-1 p-4',
               activeButton === 'sign-out' && 'ease-in-out duration-300 text-active-blue'
             ]"
-            @click="() => (activeButton = 'sign-out')"
+            @click="signOutUser"
           >
             <fontAwesome :icon="['fas', 'right-from-bracket']" />
           </button>
