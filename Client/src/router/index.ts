89dc6b6a8560cd87282ce2a62e903cdda7fa8d52
Melvin Kempe <k3mp314@gmail.com>
2025-02-15 17:37:25 +0100

    [Update] history to createWebHashHistory

--- Client/src/router/index.ts
+++ Client/src/router/index.ts
@@ -8,10 +8,10 @@ import SignInViewVue from '@/views/SignInView.vue'
 import UserHomeNewRequestViewVue from '@/views/UserHomeNewRequestView.vue'
 import UserHomeView from '@/views/UserHomeView.vue'
 import UserSentViewVue from '@/views/UserSentView.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
